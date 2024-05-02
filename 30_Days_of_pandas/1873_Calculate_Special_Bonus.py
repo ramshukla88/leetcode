eec0fabc63e15754d6641d6f2967545ba935287d
ramshukla88 <ram007maan@gmail.com>
2024-05-02 16:22:46 +0530

    Create 1873_Calculate_Special_Bonus.py
    good with both time and space complexity

---
+++ 30_Days_of_pandas/1873_Calculate_Special_Bonus.py
@@ -0,0 +1,10 @@
+# Date: 02-May-2024
+# Author: Ram Shukla
+# Location: Hyderabad, India
+
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = 0
+    employees.loc[(employees["employee_id"]%2!=0) & (employees["name"].str.startswith("M") == False), "bonus"] = employees["salary"]
+    return employees[["employee_id", "bonus"]].sort_values(by="employee_id")
