fd5fa0d59a07e9f866caa453b85a0cc34f0bb863
ramshukla88 <ram007maan@gmail.com>
2024-05-04 04:44:10 +0530

    Optimized solution
    Optimized solution

---
+++ Weekly_contest_395/3131_find_the_integer_to_array_1.py
@@ -0,0 +1,10 @@
+# Date: 02-May-2024
+# Author: Ram Shukla
+# Location: Hyderabad, India
+# When using in leet code replace print statements with return statements
+
+nums1 = [2,6,4]
+nums2 = [9,7,5]
+nums1 = max(nums1)
+nums2 = max(nums2)
+print(nums2 - nums1)
