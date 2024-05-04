7fbf0e148024c088a88e23f07481e966a3251b02
ramshukla88 <ram007maan@gmail.com>
2024-05-05 00:47:15 +0530

    Optimized code

---
+++ Daily_Practice/58_length_of_last_word.py
@@ -0,0 +1,9 @@
+# Date: 05-May-2024
+# Author: Ram Shukla
+# Location: Hyderabad, India
+# When using in leet code replace print statements with return statements
+
+s = "luffy is still joyboy"
+l = s.split()
+print(len(l[-1]))
+# return len(l[-1])
