acfa6ba326fa33b58b7f4774f1e1b9b75dc34c40
ramshukla88 <ram007maan@gmail.com>
2024-05-07 20:47:18 +0530

    solution

---
+++ Daily_Practice/43_multipy_strings.py
@@ -0,0 +1,7 @@
+# Date: 07-May-2024
+# Author: Ram Shukla
+# Location: Hyderabad, India
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return (str(int(num1)*int(num2)))
