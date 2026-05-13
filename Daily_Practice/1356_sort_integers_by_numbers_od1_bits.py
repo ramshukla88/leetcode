8c1f77541ef55aae3b75f85176bd13173f8029f0
ramshukla88 <ram007maan@gmail.com>
2026-05-13 23:52:28 +0530

    refactor: streamline counting of 1 bits by eliminating unnecessary binary conversion

--- Daily_Practice/1356_sort_integers_by_numbers_od1_bits.py
+++ Daily_Practice/1356_sort_integers_by_numbers_od1_bits.py
@@ -1,13 +1,6 @@
 arr = [0,1,2,3,4,5,6,7,8]
-binary = []
 res = {}
 for i in arr:
-    binary.append(str(bin(i)).replace("0b", ""))
-
-for i in binary:
-    count = i.count("1")
-    if count not in res:
-        res[count] = [int(i,2)]
-    else:
-        res[count].append(int(i,2))
+    count = bin(i).count("1")
+    res.setdefault(count, []).append(i)
 print([x for key in sorted(res) for x in sorted(res[key])])
